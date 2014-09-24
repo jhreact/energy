f7a1ab6bbb4ec04739e72020a29ee5c468a141ec
taojonesin <taojonesin@users.noreply.github.com>
2014-09-24 14:05:56 -0700

    initial models

--- energy/reviews/models.py
+++ energy/reviews/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.template.defaultfilters import slugify
+
+class TimeStampedModel(models.Model):
+    """
+    Abstract base class model with auto-updating created and last modified fields
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Supplier(TimeStampedModel):
+    """ Model representing a Supplier """
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(editable=False)
+
+    def save(self, *args, **kwargs):
+        # Only gen slug when creating object, so it doesn't change
+        if not self.id:
+            self.slug = slugify(self.name)
+        super(Supplier, self).save(*args, **kwargs)
+
+
+class Review(TimeStampedModel):
+    """ Model representing a Review """
+    RATING_CHOICES = [(rating, rating) for rating in range(1, 6)]
+    STATUS_CHOICES = [(state, state) for state in ('draft', 'published')]
+
+    supplier = models.ForeignKey(Supplier)
+    rating = models.PositiveIntegerField(choices=RATING_CHOICES)
+    author = models.CharField(max_length=100)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='draft')
+    content = models.TextField()

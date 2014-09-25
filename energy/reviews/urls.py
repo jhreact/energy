5512c15a1713eb8a6f0c308a46abc63a894d82c6
taojonesin <taojonesin@users.noreply.github.com>
2014-09-25 00:02:35 -0700

    added create_review identfier to url

--- energy/reviews/urls.py
+++ energy/reviews/urls.py
@@ -5,5 +5,5 @@ from .views import SupplierListView, SupplierReviewListView, SupplierReviewCreat
 urlpatterns = patterns('',
     url(r'^$', SupplierListView.as_view(), name='suppliers'),
     url(r'^(?P<slug>[\w-]+)/$', SupplierReviewListView.as_view(), name='supplier_reviews'),
-    url(r'^(?P<slug>[\w-]+)/write$', SupplierReviewCreateView.as_view()),
+    url(r'^(?P<slug>[\w-]+)/write$', SupplierReviewCreateView.as_view(), name='create_review'),
 )

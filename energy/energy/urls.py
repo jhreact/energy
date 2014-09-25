6df83970a752a8f7d229f36747b55a853ff0fc7b
taojonesin <taojonesin@users.noreply.github.com>
2014-09-24 21:18:51 -0700

    namespaced urls for reviews app

--- energy/energy/urls.py
+++ energy/energy/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^reviews/', include('reviews.urls')),
+    url(r'^reviews/', include('reviews.urls', namespace='reviews')),
     url(r'^admin/', include(admin.site.urls)),
 )

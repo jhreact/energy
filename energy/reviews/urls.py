170b29e59b1605e18973e363f2953fe363f07261
taojonesin <taojonesin@users.noreply.github.com>
2014-09-25 02:35:33 -0700

    changed create to regular view instead of class based view

--- energy/reviews/urls.py
+++ energy/reviews/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-from .views import SupplierListView, SupplierReviewListView, SupplierReviewCreateView
+from .views import SupplierListView, SupplierReviewListView, create_review
 
 urlpatterns = patterns('',
     url(r'^$', SupplierListView.as_view(), name='suppliers'),
     url(r'^(?P<slug>[\w-]+)/$', SupplierReviewListView.as_view(), name='supplier_reviews'),
-    url(r'^(?P<slug>[\w-]+)/write$', SupplierReviewCreateView.as_view(), name='create_review'),
+    url(r'^(?P<slug>[\w-]+)/write$', create_review, name='create_review'),
 )

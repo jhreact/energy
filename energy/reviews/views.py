068a45cc8fda3508f5dff623b9ddf3e04f00780a
taojonesin <taojonesin@users.noreply.github.com>
2014-09-25 00:08:42 -0700

    review list view works

--- energy/reviews/views.py
+++ energy/reviews/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, CreateView
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import get_object_or_404
 
 from .models import Supplier, Review
 
@@ -14,8 +16,12 @@ class SupplierReviewListView(ListView):
 
     def get_queryset(self):
         slug = self.kwargs.get('slug', None)
-        return Review.objects.published.filter(supplier__slug=slug).order_by('-created')
+        return Review.objects.published().filter(supplier__slug=slug).order_by('-created')
 
+    def get_context_data(self, **kwargs):
+        context = super(SupplierReviewListView, self).get_context_data(**kwargs)
+        context['supplier'] = get_object_or_404(Supplier, slug=self.kwargs.get('slug'))
+        return context
 
 class SupplierReviewCreateView(CreateView):
     model = Review

6df8ecff3f860cba897171ffaea039b778c74a08
taojonesin <taojonesin@users.noreply.github.com>
2014-09-25 13:26:28 -0700

    added comments to tests

--- energy/reviews/tests/test_models.py
+++ energy/reviews/tests/test_models.py
@@ -9,18 +9,21 @@ class SupplierModelTestCase(TestCase):
         self.ts3 = Supplier.objects.get_or_create(name="Test Supplier 3")[0]
 
     def test_has_slug(self):
-        """ Suppliers slugs are generated correctly """
+        """ Supplier slugs are generated correctly """
         self.assertEqual(self.ts1.slug, "test-supplier-1")
         self.assertEqual(self.ts2.slug, "test-supplier-2")
         self.assertEqual(self.ts3.slug, "test-supplier-3")
 
     def test_has_created(self):
+        """ Supplier object has created attribute """
         assert(hasattr(self.ts1, 'created'))
 
     def test_created_is_not_none(self):
+        """ Supplier created attribute is not null """
         self.assertIsNotNone(self.ts1.created)
 
     def test_ts1_created_less_than_ts3_created(self):
+        """ Supplier created attributes follow creation time precedence"""
         self.assertLess(self.ts1.created, self.ts3.created)
 
 
@@ -35,14 +38,18 @@ class ReviewModelTestCase(TestCase):
         self.tr1s5 = Review.objects.get_or_create(supplier=self.ts5, rating=5, author="Author 4", content="review1 for supplier5")[0]
 
     def test_has_created(self):
+        """ Review object has created attribute """
         assert(hasattr(self.tr1s4, 'created'))
 
     def test_created_is_not_none(self):
+        """ Review created attribute is not null """
         self.assertIsNotNone(self.tr1s4.created)
 
     def test_tr1s4_created_less_than_tr2s4_created(self):
+        """ Review created attributes follow creation time precedence"""
         self.assertLess(self.tr1s4.created, self.tr2s4.created)
 
-    def test_default_is_draft(self):
+    def test_status_default_is_draft(self):
+        """ Review status defaults to draft """
         self.assertEqual(self.tr1s4.status, 'draft')
 

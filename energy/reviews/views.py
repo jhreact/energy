b8d1deaf3d134fc64fc107d9b7d7093597baf8f2
taojonesin <taojonesin@users.noreply.github.com>
2014-09-24 21:20:10 -0700

    first pass at class based views

--- energy/reviews/views.py
+++ energy/reviews/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView
 
-# Create your views here.
+from .models import Supplier, Review
+
+class SupplierListView(ListView):
+    model = Supplier
+
+    def get_queryset(self):
+        return Supplier.objects.order_by('name')
+
+
+class SupplierReviewListView(ListView):
+    model = Review
+
+    def get_queryset(self):
+        slug = self.kwargs.get('slug', None)
+        return Review.objects.published.filter(supplier__slug=slug).order_by('-created')
+
+
+class SupplierReviewCreateView(CreateView):
+    model = Review
+    # slug = self.kwargs.get('slug', None)

eb2c5de539bf9f258cd86d9c4cf498d553d08564
taojonesin <taojonesin@users.noreply.github.com>
2014-09-24 21:19:44 -0700

    added named identifiers to review urls

--- energy/reviews/urls.py
+++ energy/reviews/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from .views import SupplierListView, SupplierReviewListView, SupplierReviewCreateView
 
 urlpatterns = patterns('',
-    url(r'^$', SupplierListView.as_view()),
-    url(r'^(?P<slug>[\w-]+)/$', SupplierReviewListView.as_view()),
+    url(r'^$', SupplierListView.as_view(), name='suppliers'),
+    url(r'^(?P<slug>[\w-]+)/$', SupplierReviewListView.as_view(), name='supplier_reviews'),
     url(r'^(?P<slug>[\w-]+)/write$', SupplierReviewCreateView.as_view()),
 )
